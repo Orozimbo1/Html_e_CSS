dab422fc092d436e792cfa4c0cdd744cfefd7ee6
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-04-26 19:16:28 -0300

    Deixando a barra de navegação responsiva

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('ola')
+$( document ).ready(function() {
+
+  let btnMenuMobile = $('.btn-menumobile')
+
+  $(btnMenuMobile).on('click', function() {
+
+    $('.nav-container ul').toggleClass('open')
+
+  })
+
+})
